e5e76fee7516002d0cf026715367517312934d5b
largomst <dingrui.zhen@gmail.com>
2019-09-26 17:36:26 +0800

    添加派生机制 close #3

--- Chapter.13.py
+++ Chapter.13.py
@@ -8,7 +8,10 @@ class Monster:
 
 
 class Breed:
-    def __init__(self, health: int, attack: function):
+    def __init__(
+        self, health: int = None, attack: function = None, parent: Breed = None
+    ):
+        self._parent = parent  # 用 parent 来实现非面向对象机制的派生机制。
         self._health = health
         self._attack = attack
 
@@ -19,8 +22,14 @@ class Breed:
         return Monster(self)
 
     def getHealth(self):
-        return self._health
+        if self._health != 0 or self._parent == None:
+            return self._health
+        else:
+            return self._parent._health
 
     def getAttack(self):
-        return self._attack
+        if self._attack != None or self._parent == None:
+            return self._attack
+        else:
+            return self._parent._attack
 
