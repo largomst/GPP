f1eb0d84f5c7b0a36dac872922cee00040665fb1
largomst <dingrui.zhen@gmail.com>
2019-09-26 16:41:30 +0800

    通过调用类自身的构造函数来创建新的实例

--- Chapter.13.py
+++ Chapter.13.py
@@ -12,6 +12,12 @@ class Breed:
         self._health = health
         self._attack = attack
 
+    def newMonster(self):
+        """
+        产生新 Moster 实例的工厂方法，使用“类型”来产生一个类型
+        """
+        return Monster(self)
+
     def getHealth(self):
         return self._health
 
