373708eb2d2a77edc26f38c4939f572ea71085cf
largomst <dingrui.zhen@gmail.com>
2019-09-26 16:40:23 +0800

    使用新模式 close #2

--- Chapter.13.py
+++ Chapter.13.py
@@ -1,23 +1,20 @@
 class Monster:
-    def __init__(self, startingHealth: int):
-        self._heath = startingHealth
+    def __init__(self, breed: Breed):
+        self._heath = breed.getHealth()
+        self._breed = breed
 
     def getAttack(self):
-        return 0
+        return self._breed.getAttack()
 
 
-class Dragon(Monster):
-    def __init__(self, startingHealth=230):
-        super().__init__(startingHealth)
+class Breed:
+    def __init__(self, health: int, attack: function):
+        self._health = health
+        self._attack = attack
 
-    def getAttack(self):
-        return "The dragon breathes fire!"
-
-
-class Troll(Monster):
-    def __init__(self, startingHealth=48):
-        super().__init__(startingHealth)
+    def getHealth(self):
+        return self._health
 
     def getAttack(self):
-        return "The troll clubs you!"
+        return self._attack
 
